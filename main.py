b6a1da79cb24afd12f7ad13cad0087cbd6d18131
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-22 11:50:47 +0100

    coding solution completed

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-
+#Write your code below this line 👇
+print("Welcome to Coin Toss Program")
+#Hint: Remember to import the random module first. 🎲
+import random
+# Plan
+# Generate random whole number between 1 and 0 and saved to a variable[done]
+random_number = random.randint(0,1)
+# check random number is working correctly[done]
+# print(random_number);
+# write a if/ else satement for if random number equals 0 then print Heads else print Tails
+if random_number == 0:
+  print("Heads")
+else:
+  print("Tails")
